b07f938e6443a8b56fd66c6ca0a0fbc7915b2780
liqun <liqunjin@hotmail.com>
2024-02-01 10:32:55 -0600

    Region 2+4

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/game/autonomous/Trajectories_Region.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/game/autonomous/Trajectories_Region.java
@@ -62,6 +62,35 @@ public class Trajectories_Region {
         return sequence;
     }
 
+    public TrajectorySequence getBlueRight(TeamPropDetector.TSEDetectorPipeline.TSEPosition position, Pose2d startPose) {
+
+        TrajectorySequence sequence;
+
+        if (position.equals(TeamPropDetector.TSEDetectorPipeline.TSEPosition.CENTER)) {
+            sequence = drive.trajectorySequenceBuilder(startPose)
+                    .setReversed(true)
+                    .splineToLinearHeading(new Pose2d(-43,41, Math.toRadians(120)), Math.toRadians(120))
+                    .setReversed(false)
+                    .build();
+        }
+        else if (position.equals(TeamPropDetector.TSEDetectorPipeline.TSEPosition.LEFT)) {
+            sequence = drive.trajectorySequenceBuilder(startPose)
+                    .setReversed(true)
+                    .splineTo(new Vector2d(-32,40), Math.toRadians(-50))
+                    .build();
+        }
+        else {
+            sequence = drive.trajectorySequenceBuilder(startPose)
+                    .setReversed(true)
+                    .splineTo(new Vector2d(-43, 46), Math.toRadians(245))
+                    .setReversed(false)
+                    .splineToLinearHeading(new Pose2d(-36, 51, Math.toRadians(90)), Math.toRadians(90))
+                    .build();
+        }
+
+        return sequence;
+    }
+
     public TrajectorySequence getRedRight(TeamPropDetector.TSEDetectorPipeline.TSEPosition position, Pose2d startPose) {
 
         TrajectorySequence sequence;
