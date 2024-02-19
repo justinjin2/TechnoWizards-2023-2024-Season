283dee8e4a3034db09d1e8e52fcd3cb087204f57
liqun <liqunjin@hotmail.com>
2024-02-18 20:00:54 -0600

    Region 2+4

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Claw.java
@@ -23,7 +23,7 @@ public class Claw {
     public double clawAngleCenter = 0.5;
 
     public double clawAngleIntake = 0.215;
-    public double clawAngleDeliveryStage1 = 0.16;
+    public double clawAngleDeliveryStage1 = 0.15;
     public double clawAngleDeliveryStage2 = 0.16; //used by 4bar return to intake
     public double clawAngleToHeight = 0.02; //steps when alide angle goes up
     public double clawAngleInit = 0.07;
